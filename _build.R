5e8c41c6af985757de83170a301ee0cebf45b90d
Luke W. Johnston <lwjohnst@gmail.com>
2021-09-03 16:22:23 +0200

    finished making website for dst application

---
+++ _build.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+# Need this to fix a warning/error (at least for this version)
+Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc')
+cli::cli_alert_info("Starting to render the documents.")
+
+# Build website
+rmarkdown::render_site(encoding = "UTF-8", quiet = TRUE)
+cli::cli_alert_success("Website has been built!")
+
+# Build DST Word docs
+rmarkdown::render("description.Rmd",
+            output_format = rmarkdown::word_document(
+                reference_docx = "resources/templates/description.docx"
+            ),
+            output_dir = "public",
+            quiet = TRUE
+)
+
+rmarkdown::render("appendix.Rmd",
+            output_format = rmarkdown::word_document(
+                reference_docx = "resources/templates/appendix.docx"
+            ),
+            output_dir = "public",
+            quiet = TRUE
+)
+
+cli::cli_alert_success("Word documents have been generated!")
+cli::cli_alert_info("All documents have been rendered.")
