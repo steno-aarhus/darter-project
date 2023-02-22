19f072248f31fcc1dc2bfa3833c0b0f92cfe5e12
Luke W. Johnston <lwjohnst@gmail.com>
2023-02-22 15:55:57 +0100

    moved this from index file

--- R/functions.R
+++ R/functions.R
@@ -25,3 +25,42 @@ compare_current_vs_needed_dst_registers <-
     )
     }
 
+signature_table <- function(name, position) {
+  table_prep <- tribble(
+    ~base, ~values,
+    "Navn:", name,
+    "Stilling:", position,
+    "Dato:", "",
+    "Underskrift:", ""
+  )
+
+  table_prep %>%
+    flextable::flextable() %>%
+    flextable::delete_part() %>%
+    flextable::border_remove() %>%
+    flextable::width(width = c(2, 4.5))
+}
+
+authorized_researchers_table <- function(data) {
+  flextable::flextable(data) %>%
+    flextable::border_inner() %>%
+    flextable::border_outer() %>%
+    flextable::bold(part = "header") %>%
+    flextable::valign(valign = "top", part = "header") %>%
+    flextable::bg(bg = "#d9d9d9", part = "header") %>%
+    flextable::bg(i = c(1, 3), bg = "#f2f2f2") %>%
+    flextable::bold(i = c(1, 3)) %>%
+    flextable::merge_h_range(
+      i = c(1, 3),
+      j1 = 1,
+      j2 = ncol(authorized_researchers)
+    ) %>%
+    flextable::fontsize(size = 9, part = "all") %>%
+    {
+      if (!knitr::is_html_output()) {
+        flextable::font(., fontname = "Bitstream Vera Sans", part = "all")
+      } else {
+        .
+      }
+    }
+}
